c99552c47a322d126cf37fd7692bc9d8af1d5282
doctor <fredyvides@gmail.com>
2021-05-22 13:28:07 +0000

    Julia/

---
+++ Julia/NSIdentifier.jl
@@ -0,0 +1,40 @@
+using DataFrames,CSV,LinearAlgebra,BenchmarkTools,Plots,DelimitedFiles
+
+include("spsolver.jl")
+
+@elapsed df=CSV.read("GFUdata.csv",DataFrame;header=0) |> Matrix{Float64}
+
+rk=380;
+
+df0=df[:,1:rk];
+df1=df[:,2:(rk+1)];
+
+@elapsed A=df0\df1
+
+@elapsed A0=spsolver(df0,df1,rk,5e-3,5e-3)
+
+u0=df0[:,1];                            
+u1=u0;
+u0=df0\u0;  
+M=size(df,2); 
+Ak=A0;
+for k=1:(M-1)
+    global u1=[u1 df0*(Ak*u0)];
+    global Ak=A0*Ak;
+end
+writedlm("PGFUdata0.csv",u1,',');
+u2=df0*u0;  
+Ak=A;
+for k=1:(M-1) 
+    global u1=[u1 df0*(Ak*u0)];
+    global Ak=A*Ak;
+end
+writedlm("PGFUdata1.csv",u2,',');
+
+@elapsed l=eigvals(A)
+
+@elapsed l0=eigvals(A0)
+
+scatter(real(l),imag(l))
+
+scatter(real(l0),imag(l0))
---
+++ Julia/spsolver.jl
@@ -0,0 +1,43 @@
+#Created on Wed Mar 31 02:57:52 2021
+#SPSOLVER  Sparse linear regression solver
+#   Code by Fredy Vides
+#   For Paper, "Sparse system identification by low-rank approximation"
+#   by F. Vides
+function spsolver(A,Y,L,tol,delta)
+    M=size(A,2);
+    N=size(Y,2);
+    X=zeros(M,N);
+    u,s,v=svd(A);
+    rk=sum(s.>tol);
+    u=u[:,1:rk];
+    s=s[1:rk];
+    s=s.^(-1);
+    s=diagm(s);
+    v=v[:,1:rk];
+    A=u'*A;
+    Y=u'*Y;
+    X0=v*(s*Y);
+    w=zeros(M,1);
+    for k=1:N
+        K=1;
+	    c=X0[:,k];
+	    x0=c;
+	    ac=abs.(c);
+	    f=sortperm(-ac[:,1]);
+	    N0=max(sum(ac[f].>delta),1);
+        Error = 1+tol;
+        while (K<=L) && (Error>tol)
+            ff=sort(f[1:N0,1]);
+            X[:,k]=w;
+            c=A[:,ff]\Y[:,k];
+            X[ff,k]=c;
+            Error=norm(X[:,k]-x0,Inf);
+            x0=X[:,k];
+            ac=abs.(x0);
+            f=sortperm(-ac[:,1]);
+            N0=max(sum(ac[f].>delta),1);
+            K=K+1;
+        end
+    end
+    return X
+end
